34cb827c923efe0976673391021b94fd7f9852c9
harikn77 <hariknair77@gmail.com>
2020-06-08 13:29:51 +0530

    remove eclipse/docker-compose.yml

--- eclipse/start_eclipse.sh
+++ eclipse/start_eclipse.sh
@@ -1,4 +1,7 @@
-xhost +
-echo "Starting Eclipse"
-mkdir -p ~/Workspace/EclipseProjects
-docker-compose up -d
+xhost +                                                                                                                                           
+echo "Starting Eclipse"    
+
+docker run -ti -d -e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v ~/Workspace/EclipseProjects:/home/developer/workspace/ \
+eclipse eclipse/eclipse  
